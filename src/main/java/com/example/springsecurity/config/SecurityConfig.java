d3415fa1b30eb3b58accf81f0e4be1c722e557cb
lhseunge <lhseunge@gmail.com>
2022-06-14 21:52:24 +0900

    securityFilterChain 작성

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.cors().and() //이 구문은 안에 들어가보면 주석으로 내용이 나와있지만 CorsFilter라는 필터가 존재하는데 이를 활성화 시키는 작업입니다.
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers()
+                .permitAll()
+                .antMatchers()
+
+        return httpSecurity;
+    }
+}
---
+++ src/main/java/com/example/springsecurity/controller/SecurityController.java
@@ -0,0 +1,29 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security/v1")
+public class SecurityController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+
+        return "Hello, World !!!";
+    }
+
+
+    @GetMapping("/auth")
+    public String getAuth() {
+
+        return "sueccess auth !!!";
+    }
+
+    @GetMapping("/pass")
+    public String getPass() {
+
+        return "passed auth ...";
+    }
+}
